534ca7b3cb6198734143a2e5358753f8cc83de74
MatheusAndradeMelo <mandrademelo77@gmail.com>
2021-11-05 23:45:06 -0300

    Nosso primeiro commit - Criação do projeto ODT service

---
+++ src/main/java/br/org/serratec/backend/Projeto07DtoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.org.serratec.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto07DtoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto07DtoServiceApplication.class, args);
+	}
+
+}
